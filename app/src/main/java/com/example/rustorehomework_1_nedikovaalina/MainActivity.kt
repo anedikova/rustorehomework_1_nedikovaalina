8bb2f6b55782d4680ec9ab06b4518f9916bc0da0
malinochka <azona.alinabkru@gmail.com>
2026-03-05 22:22:14 +0300

    ДЗ-1: Недикова Алина

---
+++ app/src/main/java/com/example/rustorehomework_1_nedikovaalina/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.rustorehomework_1_nedikovaalina
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rustorehomework_1_nedikovaalina.ui.theme.RuStoreHomework_1_NedikovaAlinaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RuStoreHomework_1_NedikovaAlinaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RuStoreHomework_1_NedikovaAlinaTheme {
+        Greeting("Android")
+    }
+}
